fd694d28402b3aa4b26d6e6d8d33c2c23501d351
Dustin Pfister <dustin.pfister@gmail.com>
2016-01-06 18:58:50 -0500

    ip address logging

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ openShift = {};
 //  Set the environment variables we need.
 openShift.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 openShift.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-openShift.mongo = 'mongodb://localhost/simplecount'; // default to local
+openShift.mongo = 'mongodb://localhost/openshift_demo'; // default to local
 
 // if offline
 if (typeof openShift.ipaddress === "undefined") {
@@ -48,46 +48,105 @@ simpleCount = db.model('simplecount', new Schema({
     id: String,
     count: Number
 
+})),
+iplogger = db.model('iplogger', new Schema({
+
+    ip: String,                  // the ip address
+    visitCount: Number,       // visit count from that ip
+
+/*
+    userAgents: [             // list of user agents from that ip
+        
+        new Schema({
+
+            userAgentString : String,   // the user agent string
+            visitCount : Number         // number of visits from the ip with this user agent
+
+        })
+
+    ]
+*/
+
 }));
 
 // root path get requests
 app.get('/', function(req, res){
 
-    var newInfo, displayCount = 0;
+    var newInfo, 
+    displayCount = 0,
+    userIP = req.connection.remoteAddress;
 
-    // find count
-    simpleCount.findOne({'id':'main'}, '', function(err, count){
+    // find record for ip address
+    iplogger.findOne({'ip': userIP}, '', function(err, log){
 
-        // if found add to count
-        if(count){
+        // if there is a log for the ip
+        if(log){
 
-            count.count += 1;
-            displayCount = count.count;
+            log.visitCount += 1;
+            log.save(function(){
 
-            count.save(function(){
-
-                console.log('new count saved');
+                console.log('visit # '+ log.visitCount + ' from ipaddress: ' + log.ip + ' logged!');
 
             });
 
-        // set up new record
+        // else there is not a log, so make one
         }else{
 
-            newInfo = new simpleCount({id: 'main', count: 1});
-            newInfo.save(function(){
+            log = new iplogger({
 
-                console.log('saved new count');
+                ip : userIP,
+                visitCount : 1
 
             });
 
-            displayCount = 1;
+            log.save(function(){
+
+                console.log('new ip record!');
+
+            });
 
         }
+
+
+        // find count
+        simpleCount.findOne({'id':'main'}, '', function(err, count){
+
+            // if found add to count
+            if(count){
+
+                count.count += 1;
+                displayCount = count.count;
+
+                count.save(function(){
+
+                    console.log('new count saved');
+
+                });
+
+            // set up new record
+            }else{
+
+                newInfo = new simpleCount({id: 'main', count: 1});
+                newInfo.save(function(){
+
+                    console.log('saved new count');
+
+                });
+
+                displayCount = 1;
+
+            }
         
-        //  send simple demo message, with visiter count.
-        res.send('<h1>hello i am dustins openshift_demo app working at openshift</h1><br> You are visiter #: '+ displayCount);
+            //  send simple demo message, with visiter count.
+            res.send( ' <h1>hello i am dustins openshift_demo app working at openshift</h1>'+
+            '<p> You are visiter #: '+ displayCount + '</p>'+
+            '<p> Hello visiter from ip: ' + log.ip + ' this is the ' + log.visitCount + ' visit i have logged from this ip address.</p>');
+
+        });
+
 
     });
+
 
 });
 
