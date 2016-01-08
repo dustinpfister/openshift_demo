68e7f84f2fa7b4405d48ec38d56d389b1f563732
Dustin Pfister <dustin.pfister@gmail.com>
2016-01-08 16:31:30 -0500

    os_count.js

---
+++ os_count.js
@@ -0,0 +1,214 @@
+var mongoose = require('mongoose'),
+
+    // setup the openShift Object
+    openShift = {
+
+        // get the string needed to make a connection to mono
+        mongo: (function() {
+
+            // if deployed to openshift get the string based on enviornment variables, else assume local
+            return process.env.OPENSHIFT_MONGODB_DB_PASSWORD ? process.env.OPENSHIFT_MONGODB_DB_USERNAME +
+                ":" + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+                process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+                process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+                process.env.OPENSHIFT_APP_NAME : 'mongodb://localhost/openshift_demo';
+
+        }())
+
+    },
+
+
+    db = mongoose.createConnection(openShift.mongo),
+    Schema = mongoose.Schema,
+    iplogger = db.model('iplogger', new Schema({
+
+        ip: String, // the ip address
+        visitCount: Number, // visit count from that ip
+
+
+        userAgents: [ // list of user agents from that ip
+
+            new Schema({
+
+                userAgentString: String, // the user agent string
+                visitCount: Number // number of visits from the ip with this user agent
+
+            })
+
+        ]
+
+
+    })),
+
+    // the osCount object
+    osCount = {
+
+        // patterns to look for in user agent strings
+        pats: [
+
+            {
+                osName: 'Android',
+                pat: /android/,
+                color: 'orange'
+            }, {
+                osName: 'Linux',
+                pat: /linux/,
+                color: 'yellow'
+            }, {
+                osName: 'Windows',
+                pat: /windows/,
+                color: 'green'
+            },
+
+            {
+                osName: 'total',
+                color: 'black'
+            }, {
+                osName: 'Other',
+                color: 'grey'
+            }
+
+        ],
+
+        // the count
+        count: {},
+        ipCount: 0,
+
+        // count reset
+        reset: function() {
+
+            this.count = {};
+            this.ipCount = 0;
+            this.count.total = {
+                count: 0,
+                color: 'black'
+            }
+
+        },
+
+        // count a user agent string
+        countUA: function(UA) {
+
+            var i = 0,
+                len = this.pats.length,
+                osName;
+
+            this.count.total.count += 1;
+
+            while (i < len) {
+
+                if (UA.toLowerCase().match(this.pats[i].pat)) {
+
+                    osName = this.pats[i].osName;
+                    if (this.count[osName] === undefined) {
+                        this.count[osName] = {
+                            count: 1,
+                            color: this.pats[i].color
+                        }
+                    } else {
+                        this.count[osName].count += 1;
+                    }
+
+                    break;
+
+                }
+
+                i++;
+
+            }
+
+            if (i === len) {
+                if (this.count['Other'] === undefined) {
+                    this.count['Other'] = {
+                        count: 1,
+                        color: 'red'
+                    }
+                } else {
+
+                    this.count['Other'].count += 1;
+
+                }
+            }
+
+        },
+
+        // the update loop
+        update: function() {
+
+            var i, a, aLen, agents, self = this;
+
+            //  update every once in a while
+            setTimeout(osCount.update.bind(osCount), 5000);
+
+            // update stuff
+            iplogger.find(function(err, logs) {
+
+                self.reset();
+
+                // set the new ipCount
+                self.ipCount = logs.length;
+
+                // find new OS count
+                i = 0;
+                while (i < self.ipCount) {
+
+                    agents = logs[i].userAgents;
+                    a = 0;
+                    aLen = agents.length;
+
+                    while (a < aLen) {
+
+                        self.countUA(agents[a].userAgentString);
+
+                        a++;
+                    }
+
+                    i++;
+                }
+
+                console.log('ipCount: ' + self.ipCount);
+                console.log(self.count);
+
+            });
+
+
+
+        }
+
+    };
+
+
+
+exports.startUpdate = function() {
+
+    //   setTimeout(exports.update, 1000);
+    osCount.update();
+
+};
+
+exports.makeHTML = function() {
+
+    var html = '<ul>';
+
+    for (var os in osCount.count) {
+        html += '<li style="color:' + osCount.count[os].color + ';">' + os + ' : ' + osCount.count[os].count + '</li>';
+    }
+
+
+    html += '</ul><div style="width:400px;height:30px;background:#888888;">';
+
+    for (osName in osCount.count) {
+        if (osName !== 'total') {
+            html += '<div style="display:inline-block; width:' + Math.floor(osCount.count[osName].count / osCount.count['total'].count * 400) + 'px; height:30px; background:' + osCount.count[osName].color + ';"></div>';
+        }
+    }
+
+    return html + '</div>';;
+
+};
+
+exports.getIPCount = function(){
+
+    return osCount.ipCount;
+
+};
--- server.js
+++ server.js
@@ -7,110 +7,15 @@
  */
 
 var express = require('express'),
-mongoose = require('mongoose'),
-
-ipCount = 0, // number of ip address logged
-
-osPats = [
-    {osName: 'Android', pat: /android/, color: 'orange' },
-    {osName: 'Linux', pat: /linux/, color: 'yellow' },
-    {osName: 'Windows', pat: /windows/, color: 'green' },
-
-    {osName: 'total', color: 'black' },
-    {osName: 'Other', color: 'grey' }
-
-],
-
-osCount = {
-    //total : 0,
-    //android : 0,
-    //otherLinux : 0,
-    //otherOS : 0
-},
-osReset = function(){
-
-    osCount = {};
-    osCount.total = {
-        count: 0,
-        color: 'black'
-    }
-
-},
-osCountUA = function(UA){
-
-    var i=0, len = osPats.length;
-
-    osCount.total.count += 1;
-
-    while(i < len){
-
-        if(UA.toLowerCase().match(osPats[i].pat)){
-
-            var osName = osPats[i].osName;
-            if(osCount[osName] === undefined){
-                osCount[osName] = {
-                    count: 1,
-                    color: osPats[i].color
-                }
-            }else{
-               osCount[osName].count += 1;
-            }
-
-            break;
-
-        }
-
-        i++;
-
-    }
-
-    if(i === len){
-        if(osCount['Other'] === undefined){
-            osCount['Other'] = {
-                count: 1,
-                color: 'red'
-            }
-        }else{
-
-            osCount['Other'].count += 1;
-
-        }
-    }
-
-},
-
-makeOSCountHTML = function(){
-
-    var html = '<ul>';
-
-    for(var os in osCount){
-        html += '<li style="color:'+osCount[os].color+';">'+os + ' : ' + osCount[os].count+'</li>';
-    }
+    mongoose = require('mongoose'),
+    os_count = require('./os_count.js'),
 
 
-    return html+'</ul>';
+    // express app
+    app = express(),
 
-},
-
-makeOSDivBar = function(){
-
-    var html='';
-
-    for(osName in osCount){
-        if(osName !== 'total'){
-            html += '<div style="display:inline-block; width:'+Math.floor(osCount[osName].count / osCount['total'].count * 400)+'px; height:30px; background:'+osCount[osName].color+';"></div>';
-        }
-    }
-
-    return '<div style="width:400px;height:30px;background:#888888;">'+html+'</div>';;
-
-},
-
-// express app
-app = express(),
-
-// to hold any openshift stuff, like environment variables.
-openShift = {};
+    // to hold any openshift stuff, like environment variables.
+    openShift = {};
 
 //  Set the environment variables we need.
 openShift.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
@@ -122,107 +27,107 @@ if (typeof openShift.ipaddress === "undefined") {
 
     // use something local such as "127.0.0.1", "localhost", or something like "192.168.1.4"
     openShift.ipaddress = '192.168.1.4';
-    console.warn('Working offline? , using '+openShift.ipaddress+' on port '+openShift.port);
+    console.warn('Working offline? , using ' + openShift.ipaddress + ' on port ' + openShift.port);
 };
 
 // mongo on openshift?
 // if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 
     openShift.mongo = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
 
 }
 
 // mongoose
-var db = mongoose.connect(openShift.mongo),
-Schema = mongoose.Schema,
-simpleCount = db.model('simplecount', new Schema({
+//var db = mongoose.connect(openShift.mongo),
+var db = mongoose.createConnection(openShift.mongo),
+    Schema = mongoose.Schema,
+    simpleCount = db.model('simplecount', new Schema({
+        id: String,
+        count: Number
 
-    id: String,
-    count: Number
+    })),
+    iplogger = db.model('iplogger', new Schema({
 
-})),
-iplogger = db.model('iplogger', new Schema({
-
-    ip: String,                  // the ip address
-    visitCount: Number,       // visit count from that ip
-
-
-    userAgents: [             // list of user agents from that ip
-        
-        new Schema({
-
-            userAgentString : String,   // the user agent string
-            visitCount : Number         // number of visits from the ip with this user agent
-
-        })
-
-    ]
+        ip: String, // the ip address
+        visitCount: Number, // visit count from that ip
 
 
-})),
+        userAgents: [ // list of user agents from that ip
+
+            new Schema({
+
+                userAgentString: String, // the user agent string
+                visitCount: Number // number of visits from the ip with this user agent
+
+            })
+
+        ]
 
 
-findAgent = function(log, userAgent){
-
-    var agents = log.userAgents,
-    i = 0;
-
-    while(i < agents.length){
-
-        if(agents[i].userAgentString === userAgent){
-
-            return agents[i];
-
-         }
-
-         i++;
-    }
-
-    return false;
-        
-};
+    })),
 
 
+    findAgent = function(log, userAgent) {
+
+        var agents = log.userAgents,
+            i = 0;
+
+        while (i < agents.length) {
+
+            if (agents[i].userAgentString === userAgent) {
+
+                return agents[i];
+
+            }
+
+            i++;
+        }
+
+        return false;
+
+    };
 
 // trust proxy
 app.enable('trust proxy');
 
 // root path get requests
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 
-    var newInfo, 
-    displayCount = 0,
-    userIP = req.ip,
-    userAgent = req.get('user-agent'),
-    userAgentVisit = 0,
-    agent, html, i;
+    var newInfo,
+        displayCount = 0,
+        userIP = req.ip,
+        userAgent = req.get('user-agent'),
+        userAgentVisit = 0,
+        agent, html, i;
 
     // find record for ip address
-    iplogger.findOne({'ip': userIP}, '', function(err, log){
+    iplogger.findOne({
+        'ip': userIP
+    }, '', function(err, log) {
 
         // if there is a log for the ip
-        if(log){
+        if (log) {
 
             // bump ip level count
             log.visitCount += 1;
 
             var agent = findAgent(log, userAgent);
 
-            if(agent){
+            if (agent) {
 
-                
+
                 agent.visitCount += 1;
 
-                console.log('bumping count for user agent: '+ agent.visitCount);
+                console.log('bumping count for user agent: ' + agent.visitCount);
 
                 userAgentVisit = agent.visitCount;
 
-            }else{
+            } else {
 
                 console.log('new user agent for ip: ' + log.ip);
                 log.userAgents.push({
@@ -235,19 +140,19 @@ app.get('/', function(req, res){
             }
 
             // save / update log
-            log.save(function(){
+            log.save(function() {
 
-                console.log('visit # '+ log.visitCount + ' from ipaddress: ' + log.ip + ' logged!');
+                console.log('visit # ' + log.visitCount + ' from ipaddress: ' + log.ip + ' logged!');
 
             });
 
-        // else there is not a log, so make one
-        }else{
+            // else there is not a log, so make one
+        } else {
 
             log = new iplogger({
 
-                ip : userIP,
-                visitCount : 1,
+                ip: userIP,
+                visitCount: 1,
                 userAgents: []
 
             });
@@ -257,7 +162,7 @@ app.get('/', function(req, res){
                 visitCount: 1
             });
 
-            log.save(function(){
+            log.save(function() {
 
                 console.log('new ip record!');
 
@@ -267,25 +172,30 @@ app.get('/', function(req, res){
 
 
         // find count
-        simpleCount.findOne({'id':'main'}, '', function(err, count){
+        simpleCount.findOne({
+            'id': 'main'
+        }, '', function(err, count) {
 
             // if found add to count
-            if(count){
+            if (count) {
 
                 count.count += 1;
                 displayCount = count.count;
 
-                count.save(function(){
+                count.save(function() {
 
                     console.log('new count saved');
 
                 });
 
-            // set up new record
-            }else{
+                // set up new record
+            } else {
 
-                newInfo = new simpleCount({id: 'main', count: 1});
-                newInfo.save(function(){
+                newInfo = new simpleCount({
+                    id: 'main',
+                    count: 1
+                });
+                newInfo.save(function() {
 
                     console.log('saved new count');
 
@@ -294,45 +204,31 @@ app.get('/', function(req, res){
                 displayCount = 1;
 
             }
-        
 
-           agent = log.userAgents, html='',i=0;
-           while(i < agent.length){
-               html += '<ul><li>'+agent[i].userAgentString + ' </li><li>' + agent[i].visitCount+'</li></ul>';;
-               i++;
-           }
+
+            agent = log.userAgents, html = '', i = 0;
+            while (i < agent.length) {
+                html += '<ul><li>' + agent[i].userAgentString + ' </li><li>' + agent[i].visitCount + '</li></ul>';;
+                i++;
+            }
 
             //  send simple demo message, with visiter count.
-            res.send( ' <h1>hello i am dustins openshift_demo app working at openshift</h1><br><br>'+
-            '<h2>Visit Count</h2>'+
-            '<p> You are visiter #: '+ displayCount + '</p>'+
+            res.send(' <h1>hello i am dustins openshift_demo app working at openshift</h1><br><br>' +
+                '<h2>Visit Count</h2>' +
+                '<p> You are visiter #: ' + displayCount + '</p>' +
 
-             '<h2>OS Count</h2>'+
+                '<h2>OS Count</h2>' +
 
-             makeOSCountHTML() +
-             makeOSDivBar()+
+                os_count.makeHTML() +
 
-
-//             '<p style="color:#ffaa00;">Android: '+osCount.android+'</p>'+
-//             '<p style="color:#ffff00;">Other Linux: '+osCount.otherLinux+'</p>'+
-//             '<p style="color:#888888;">Other OS / Unkown: '+osCount.otherOS+'</p>'+
-             
-             //'<div style="width:300px; height:30px; background:#888888;">'+
-
-//                 '<div style="display:inline-block; width:'+Math.floor(osCount.android / osCount.total * 300)+'px; height:30px; background:#ffaa00;"></div>'+
-//                 '<div style="display:inline-block; width:'+Math.floor(osCount.otherLinux / osCount.total * 300)+'px; height:30px; background:#ffff00;"></div>'+
-
-             //'</div>'+
-
-
-            '<h2> unique ip count: </h2>'+
-            '<p> I have logged '+ipCount+' unique ip address in my database.</p>'+
-            '<p> req.get(\'host\'): '+req.get('host')+'</p>'+
-            '<p> req.ip: '+req.ip+'</p>'+
-            '<p> req.connection.remoteAddress: '+req.connection.remoteAddress+'</p>'+
-            '<p> YOU are a visiter from the ip (log.ip from req.ip)  ' + log.ip + ', and i have a visit count of ' + log.visitCount + ' from this ip address.</p>'+
-            '<h2> User agent history from this ip: </h2>'+
-            html
+                '<h2> unique ip count: </h2>' +
+                '<p> I have logged ' + os_count.getIPCount() + ' unique ip address in my database.</p>' +
+                '<p> req.get(\'host\'): ' + req.get('host') + '</p>' +
+                '<p> req.ip: ' + req.ip + '</p>' +
+                '<p> req.connection.remoteAddress: ' + req.connection.remoteAddress + '</p>' +
+                '<p> YOU are a visiter from the ip (log.ip from req.ip)  ' + log.ip + ', and i have a visit count of ' + log.visitCount + ' from this ip address.</p>' +
+                '<h2> User agent history from this ip: </h2>' +
+                html
             );
 
         });
@@ -343,87 +239,8 @@ app.get('/', function(req, res){
 
 });
 
-var update = function(){
-
-    var i, a, aLen, agents;
-
-    //  update every once in a while
-    setTimeout(update, 10000);
-
-    // update stuff
-    iplogger.find(function(err, logs){
-
-        // set the new ipCount
-        ipCount = logs.length;
-        
-        i=0;
-
-        // reset os count
-        //osCount = {
-      //      total : 0,
-      //      android : 0,
-      //      otherLinux : 0,
-      //      otherOS : 0
-
-        //};
-
-        //osCount.total = 0;
-
-        osReset();
-
-        // find new OS count
-        while(i < ipCount){
-
-            agents = logs[i].userAgents;
-            a = 0; aLen = agents.length;
-
-            while(a < aLen){
-                //console.log('    '+agents[a].userAgentString);
-
-               // osCount.total += 1;
-
-                osCountUA(agents[a].userAgentString);
-
-/*
-                // is linux?
-                if(agents[a].userAgentString.toLowerCase().match(/linux/)){
-
-                    // android?
-                    if(agents[a].userAgentString.toLowerCase().match(/android/)){
-
-                        osCount.android += 1;
-
-                    // other linux? :-)
-                    }else{
-
-                        osCount.otherLinux += 1;
-
-                    }
- 
-                // other os, no idea? :-(
-                }else{
-
-                    osCount.otherOS += 1;
-
-                }
-*/
-
-                a++;
-            }
-
-            i++;
-        }
-
-        console.log(osCount);
-
-    });
-
-};
-
-
-
 // start server
 app.listen(openShift.port, openShift.ipaddress);
 
 // run update loop
-update();
+os_count.startUpdate();
